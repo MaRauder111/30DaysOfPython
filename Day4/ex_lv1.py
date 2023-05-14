c21b5a9e354b981edf09a6e536c1c26ef8f89f57
MaRauder111 <njustwant92@gmail.com>
2023-05-14 23:51:06 +0530

    day 4 done

--- Day4/ex_lv1.py
+++ Day4/ex_lv1.py
@@ -62,3 +62,41 @@ print(f'{first_}{middle_}{last_}')
 
 print(acronym_2.index('C'))
 print(acronym_2.index('F'))
+
+print(acronym_2.rfind('l'))
+
+value = 'You cannot end a sentence with because because because is a conjunction'
+print(value.index('because'))
+
+print(value.rfind('because'))
+
+print(value[31:54])
+
+substring_check = 'Coding For All'
+substring = 'Coding'
+print(substring_check.startswith(substring))
+print(substring_check.endswith('coding'))
+
+remove_space = '   Coding For All      '
+print(remove_space.strip('  '))
+
+checking_value = '30DaysOfPython'
+checking_value_1 = 'thirty_days_of_python'
+print(checking_value.isidentifier())
+print(checking_value_1.isidentifier())
+
+val = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('#'.join(val))
+
+print('I am enjoying this challenge.\nI just wonder what is next.')
+print('Name\tAge\tCountry\tCity\nAsabeneh\t250\tFinland\tHelsinki')
+
+radius = 10
+pie = 3.14
+area = pie * radius ** 2
+print('radius = {}'.format(radius))
+print('area = {} * {} ** 2'.format(pie,radius))
+print('The area of a circle with radius {} is {} meters square.'.format(radius,area))
+
+print('{} + {} = {}'.format(8, 6, 14))
+print('{} - {} = {}'.format(8,6,2))
